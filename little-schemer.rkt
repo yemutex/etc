8cea67fe4de56253452f15097cedb33b9f2ce493
Mu Ye <emailyemu@gmail.com>
2016-04-10 13:36:53 -0400

    expt and division

--- little-schemer.rkt
+++ little-schemer.rkt
@@ -160,3 +160,17 @@
       ((> n m) #f)
       ((< n m) #f)
       (else #t))))
+
+
+(define expt
+  (lambda (n m)
+    (cond
+      ((zero? m) 1)
+      (else (* n (expt n (sub1 m)))))))
+
+
+(define /
+  (lambda (n m)
+    (cond
+      ((< n m) 0)
+      (else (add1 (/ (- n m) m))))))
