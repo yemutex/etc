dda14e95f671dd96a1fc6a7e63e6d2851810e08a
Mu Ye <emailyemu@gmail.com>
2016-04-10 12:51:59 -0400

    =

--- little-schemer.rkt
+++ little-schemer.rkt
@@ -144,3 +144,19 @@
       ((zero? m) #f)
       ((zero? n) #t)
       (else (< (sub1 n) (sub1 m))))))
+
+
+(define =
+  (lambda (n m)
+    (cond
+      ((zero? m) (zero? n))
+      ((zero? n) #f)
+      (else (= (sub1 n) (sub1 m))))))
+
+
+(define =
+  (lambda (n m)
+    (cond
+      ((> n m) #f)
+      ((< n m) #f)
+      (else #t))))
