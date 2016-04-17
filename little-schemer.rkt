3e0420e2626d774443c575fdb20c0caa8157fc57
Mu Ye <emailyemu@gmail.com>
2016-04-17 00:48:14 -0400

    insertL*

--- little-schemer.rkt
+++ little-schemer.rkt
@@ -282,3 +282,14 @@
          ((eq? (car l) old) (cons new (subst* new old (cdr l))))
          (else (cons (car l) (subst* new old (cdr l))))))
       (else (cons (subst* new old (car l)) (subst* new old (cdr l)))))))
+
+
+(define insertL*
+  (lambda (new old l)
+    (cond
+      ((null? l) (quote ()))
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) old) (cons new (cons old (insertL* new old (cdr l)))))
+         (else (cons (car l) (insertL* new old (cdr l))))))
+      (else (cons (insertL* new old (car l)) (insertL* new old (cdr l)))))))
