da123932af38ea31f0a47594e46e1ebd42484fb3
Mu Ye <emailyemu@gmail.com>
2016-04-17 00:28:54 -0400

    subst*

--- little-schemer.rkt
+++ little-schemer.rkt
@@ -271,3 +271,14 @@
          ((eq? (car l) a) (add1 (occur* (cdr l))))
          (else (occur* (cdr l)))))
       (else (+ (occur* a (car l)) (occur* a (cdr l)))))))
+
+
+(define subst*
+  (lambda (new old l)
+    (cond
+      ((null? l) (quote ()))
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) old) (cons new (subst* new old (cdr l))))
+         (else (cons (car l) (subst* new old (cdr l))))))
+      (else (cons (subst* new old (car l)) (subst* new old (cdr l)))))))
