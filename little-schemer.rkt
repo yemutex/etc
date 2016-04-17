92f016333706d3ede3d493c04a3aa1c54c115507
Mu Ye <emailyemu@gmail.com>
2016-04-16 23:39:32 -0400

    insertR*

--- little-schemer.rkt
+++ little-schemer.rkt
@@ -249,3 +249,14 @@
          ((eq? a (car l)) (rember* a (cdr l)))
          (else (cons (car l) (rember* a (cdr l))))))
       (else (cons (rember* a (car l) (rember* a (cdr l))))))))
+
+
+(define insertR*
+  (lambda (new old l)
+    (cond
+      ((null? l) (quote ()))
+      ((atom? (car l))
+       (cond
+         ((eq? (car l) old) (cons old (cons new (insertR* new old (cdr l)))))
+         (else (cons (car l) (insertR* new old (cdr l))))))
+      (else (cons (insertR* new old (car l)) (insertR* new old (cdr l)))))))
