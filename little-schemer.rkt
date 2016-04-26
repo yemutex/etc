84334083889ddf3a11167bdfdc42c8678682ffb1
Mu Ye <emailyemu@gmail.com>
2016-04-25 23:53:55 -0400

    eqlist?

--- little-schemer.rkt
+++ little-schemer.rkt
@@ -313,3 +313,16 @@
     (cond
       ((atom? (car l)) (car l))
       (else (leftmost (car l))))))
+
+
+(define eqlist?
+  (lambda (l1 l2)
+    (cond
+      ((and (null? l1) (null? l2)) #t)
+      ((or (null? l1) (null? l2)) #f)
+      ; Neither l1 nor l2 is empty
+      ((and (atom? (car l1)) (atom? (car l2)))
+       (and (eqan? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2))))
+      ((or (atom? (car l1) (atom? (car l2)))) #f)
+      (else
+        (and (eqlist? (car l1) (car l2)) (eqlist? (cdr l1) (cdr l2)))))))
