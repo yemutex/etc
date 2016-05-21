f720a74e0a5e7a1ce7aee5c8bdacc41f56efef42
Mu Ye <emailyemu@gmail.com>
2016-05-21 18:27:37 -0400

    intersect

--- little-schemer.rkt
+++ little-schemer.rkt
@@ -419,3 +419,12 @@
       ((null? set1) #f)
       (else (or (member? (car set1) set2)
                 (intersect? (cdr set1) set2))))))
+
+
+(define intersect
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) (quote ()))
+      ((member? (car set1) set2) (cons (car set1)
+                                       (intersect (cdr set1) set2)))
+      (else (instersect (cdr set1) set2)))))
