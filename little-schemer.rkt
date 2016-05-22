ae475ec67a1149207f69d0f67c01397b4f6d3c53
Mu Ye <mu@tunnelbear.com>
2016-05-22 12:41:58 -0400

    intersectall

--- little-schemer.rkt
+++ little-schemer.rkt
@@ -436,3 +436,11 @@
       ((null? set1) set2)
       ((member? (car set1) set2) (union (cdr set1) set2))
       (else (cons (car set1) (union (cdr set1) set2))))))
+
+
+(define intersectall
+  (lambda (l-set)
+    (cond
+      ((null? (cdr l-set)) (car l-set))
+      (else (intersect (car l-set) (intersectall (cdr l-set))))
+    )))
