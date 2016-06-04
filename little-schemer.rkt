a84da91116ec852cf8b524847aa2e31863236585
Mu Ye <emailyemu@gmail.com>
2016-06-04 12:33:11 -0400

    rember-f curry

--- little-schemer.rkt
+++ little-schemer.rkt
@@ -500,3 +500,12 @@
       ((null? l) (quote ()))
       ((test? (car l) a) (cdr l))
       (else (cons (car l) (rember-f test? a (cdr l)))))))
+
+
+(define rember-f
+  (lambda (test?)
+    (lambda (a l)
+      (cond
+        ((null? l) (quote ()))
+        ((test? (car l) a) (cdr l))
+        (else (cons (car l) ((rember-f test?) a (cdr l))))))))
