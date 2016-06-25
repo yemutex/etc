abccb1ac19f6da4c13c3b6e443c53202a477870a
Mu Ye <emailyemu@gmail.com>
2016-06-25 00:14:04 -0400

    multiinsertLR

--- little-schemer.rkt
+++ little-schemer.rkt
@@ -586,3 +586,14 @@
         (multirember&co a (cdr lat)
                         (lambda (newlat seen)
                           (col (cons (car lat) newlat) seen)))))))
+
+
+(define multiinsertLR
+  (lambda (new oldL oldR lat)
+    (cond
+      ((null? lat) (quote ()))
+      ((eq? (car lat) oldL)
+       (cons new (cons oldL (multiinsertLR new oldL oldR (cdr lat)))))
+      ((eq? (car lat) oldR)
+       (cons oldR (cons new (multiinsertLR new oldL oldR (cdr lat)))))
+      (else (cons (car lat) (multiinsertLR new oldL oldR (cdr lat)))))))
