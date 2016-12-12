ec18fe5fb4f5194e1ef05521965c78bcfaac7401
Mu Ye <emailyemu@gmail.com>
2016-12-11 20:33:47 -0500

    General notation

---
+++ sudoku/sudoku.py
@@ -0,0 +1,16 @@
+def cross(A, B):
+    '''Cross product of elements in A and elements in B.'''
+    return [a+b for a in A for b in B]
+
+digits = '123456789'
+rows = 'ABCDEFGHI'
+cols = digits
+squares = cross(rows, cols)
+unit_list = ([cross(rows, c) for c in cols] +
+             [cross(r, cols) for r in rows] +
+             [cross(rs, cs) for rs in (
+                 'ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')])
+units = dict((s, [u for u in unit_list if s in u]) for s in squares)
+import pdb
+pdb.set_trace()
+peers = dict((s, set(sum(units[s], [])) - set([s])) for s in squares)
