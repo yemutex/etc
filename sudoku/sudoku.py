587788288837a6e159c1d0807961b7eb83bad9d4
Mu Ye <emailyemu@gmail.com>
2016-12-27 12:51:14 -0500

    Intro comment

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,3 +1,7 @@
+# Sudoku solver by Peter Norvig
+# http://norvig.com/sudoku.html
+
+
 def cross(A, B):
     '''Cross product of elements in A and elements in B.'''
     return [a+b for a in A for b in B]
