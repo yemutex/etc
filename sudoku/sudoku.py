b4294e03e9e44acee0554ffe5d276c42b39089f9
Mu Ye <emailyemu@gmail.com>
2016-12-28 18:30:34 -0500

    Unit tests

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -16,3 +16,19 @@ unit_list = ([cross(rows, c) for c in cols] +
                  'ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')])
 units = dict((s, [u for u in unit_list if s in u]) for s in squares)
 peers = dict((s, set(sum(units[s], [])) - set([s])) for s in squares)
+
+
+# Unit tests
+def test():
+    assert len(squares) == 81
+    assert len(unit_list) == 27
+    assert all(len(units[s]) == 3 for s in squares)
+    assert all(len(peers[s]) == 20 for s in squares)
+    assert units['C2'] == [
+        ['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2'],
+        ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9'],
+        ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']]
+    assert peers['C2'] == set(
+        ['A2', 'B2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2', 'C1', 'C3', 'C4',
+         'C5', 'C6', 'C7', 'C8', 'C9', 'A1', 'A3', 'B1', 'B3'])
+    print 'All tests pass.'
