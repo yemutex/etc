b606002d4651af5277cdaaa0b51a10ea93f2f404
Mu Ye <emailyemu@gmail.com>
2016-12-31 14:34:14 -0500

    eliminate

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -46,6 +46,36 @@ def assign(values, s, d):
         return False
 
 
+def eliminate(values, s, d):
+    '''Eliminate d from values[s]; propagate when values or places <= 2.
+    Return values, except return False if a contradiction is detected.'''
+    if d not in values[s]:
+        return values
+    values[s] = values[s].replace(d, '')
+
+    # 1) If a square s is reduced to one value d2, then eliminate d2 from
+    # the peers
+    if len(values[s]) == 0:
+        # Contradiction: removed last value
+        return False
+    elif len(values[s]) == 1:
+        d2 = values[s]
+        if not all(eliminate(values, s2, d2) for s2 in peers[s]):
+            return False
+    # 2) If a unit u is reduced to only one place for a value d, then
+    # put it there
+    for u in units[s]:
+        dplaces = [sq for sq in u if d in values[s]]
+        if len(dplaces) == 0:
+            # Contradiction: no place for this value
+            return False
+        elif len(dplaces) == 1:
+            # d can only be in one place in unit; assign it there
+            if not assign(values, dplaces[0], d):
+                return False
+    return values
+
+
 # Unit tests
 def test():
     assert len(squares) == 81
