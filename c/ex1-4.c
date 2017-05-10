7c5f8b13d17949a946a0405d232d6c664405410d
Mu Ye <mu@tunnelbear.com>
2017-05-10 14:13:14 -0400

    Use symbolic constant

--- c/ex1-4.c
+++ c/ex1-4.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 30
+
 int main() {
         float fahr, celsius;
-        int lower, upper;
 
-        lower = 0;
-        upper = 30;
-
-        celsius = lower;
-        while (celsius <= upper) {
+        celsius = LOWER;
+        while (celsius <= UPPER) {
                 fahr = celsius * 9 / 5 + 32;
                 printf("%.0f\t%.1f\n", celsius, fahr);
                 celsius++;
